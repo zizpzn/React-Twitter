34bd0b5a99403607017d916bd1f80ad3933e49f3
won <zizpzn@gmail.com>
2023-01-10 16:06:54 +0700

    react, firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLMUWjI32RADFBMJgTz6Krri0eYkjMaXQ",
+  authDomain: "twitter-1bf55.firebaseapp.com",
+  projectId: "twitter-1bf55",
+  storageBucket: "twitter-1bf55.appspot.com",
+  messagingSenderId: "78177513489",
+  appId: "1:78177513489:web:b45d54144a37dae2a981b0",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
