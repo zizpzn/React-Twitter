5a80cba7cb2c384fd851b2fc7a6729862ca02593
won <zizpzn@gmail.com>
2023-01-11 14:48:45 +0700

    login form part 1

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={email}
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          required
+          onChange={handleChange}
+        />
+        <input type="submit" value="Login" />
+      </form>
+      <button>Continu with Google</button>
+    </div>
+  );
 };
 
 export default Auth;
