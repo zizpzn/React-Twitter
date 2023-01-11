55c752a736693642b69bf56fe614c4c6286530e6
won <zizpzn@gmail.com>
2023-01-11 09:22:14 +0700

    env

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLMUWjI32RADFBMJgTz6Krri0eYkjMaXQ",
-  authDomain: "twitter-1bf55.firebaseapp.com",
-  projectId: "twitter-1bf55",
-  storageBucket: "twitter-1bf55.appspot.com",
-  messagingSenderId: "78177513489",
-  appId: "1:78177513489:web:b45d54144a37dae2a981b0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
