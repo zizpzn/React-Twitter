de48efe4c4031d0b617c05c66a1192597e2cf0f7
won <zizpzn@gmail.com>
2023-01-12 15:32:53 +0700

    logout, useHistory

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
+import Navigation from "./Navigation";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({ isLogin }) => {
   return (
     <Router>
+      {isLogin && <Navigation />}
       <Switch>
         {isLogin ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            {/* <Redirect from="*" to="/" /> */}
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            {/* <Redirect from="*" to="/" /> */}
+          </>
         )}
       </Switch>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { auth } from "../firebase";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const history = useHistory();
+  const logOutClick = () => {
+    auth.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={logOutClick}>Logout</button>
+    </>
+  );
 };
 
 export default Profile;
